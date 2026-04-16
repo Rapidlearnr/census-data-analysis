a61894d6bba74cddc70c4f8ce80daa13fea7d037
Md Shaad Akhtar <shanawzakahtar@gmail.com>
2026-04-16 23:48:47 +0530

    Added outlier detection using IQR and initial visualizations (histogram, boxplot, correlation heatmap)

--- project.py
+++ project.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 from scipy.stats import norm
 
 df = pd.read_excel("Census_A1_Clean.xlsx")
-# OBJECTIVE 1: EDA (including missing values check and handling)
+# EDA (including missing values check and handling)
 
 print("First 5 rows:")
 print(df.head())
@@ -31,4 +31,50 @@ print(df.isnull().sum())
 
 print("\nSummary statistics:")
 print(df.describe())
+# OBJECTIVE 1: Outlier Detection
+
+Q1 = df["Population_Total_Rural"].quantile(0.25)
+Q3 = df["Population_Total_Rural"].quantile(0.75)
+IQR = Q3 - Q1
+
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+outliers = df[(df["Population_Total_Rural"] < lower_bound) | (df["Population_Total_Rural"] > upper_bound)]
+
+print("\nOutliers in Population_Total_Rural:")
+print(outliers)
+
+print("\nNumber of outliers:")
+print(outliers.shape[0])
+
+
+# OBJECTIVE 2: Visualization Before Removing Outliers and Correlation
+
+plt.figure(figsize=(8, 5))
+sns.histplot(df["Population_Total_Rural"], bins=30, kde=True)
+plt.title("Distribution of Rural Population Before Removing Outliers")
+plt.xlabel("Population_Total_Rural")
+plt.ylabel("Frequency")
+plt.show()
+
+plt.figure(figsize=(8, 5))
+sns.boxplot(y=df["Population_Total_Rural"])
+plt.title("Boxplot of Rural Population Before Removing Outliers")
+plt.ylabel("Population_Total_Rural")
+plt.show()
+
+corr_cols = [
+    "Households_Rural",
+    "Population_Total_Rural",
+    "Households_Urban",
+    "Population_Total_Urban",
+    "Pop_per_sq_km_Rural",
+    "Pop_per_sq_km_Urban"
+]
+
+plt.figure(figsize=(10, 6))
+sns.heatmap(df[corr_cols].corr(), annot=True)
+plt.title("Correlation Heatmap")
+plt.show()
 
